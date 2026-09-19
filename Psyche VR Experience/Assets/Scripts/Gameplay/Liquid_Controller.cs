9e0ade68a11721ac95140151486a02b5b941f2d6
rjaworskiASU <rfjawors@asu.edu>
2026-09-19 14:09:10 -0400

    Added liquid to cup... started working on liquid like physics

---
+++ Psyche VR Experience/Assets/Scripts/Gameplay/Liquid_Controller.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CoffeeLiquidController : MonoBehaviour
+{
+    public Transform coffeeSurface;
+
+    void LateUpdate()
+    {
+        // Liquid-level behavior will be implemented here.
+    }
+}
